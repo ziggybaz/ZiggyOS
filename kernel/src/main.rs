dfd425102a04087147bbc0ba70e760e7a34c6fa4
Ziggy Baz <basil99magawi@gmail.com.com>
2024-04-06 05:20:32 +0300

    Sorting linker errors to omit C runtime when compiling since its freestanding

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,12 +1,20 @@
 //I'll clean up all this comments after implementation, i hate them goes against the principles of keeping code clean.
-#![no_std]
+#![no_std] //disabling std lib.
+#![no_main] //overwriting the entry-point
 
 use core::panic::PanicInfo;
-
-
-fn main() {}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+
+
+
+
+

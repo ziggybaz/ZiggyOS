458a8d557428ced6dfe9aaf2a609cbf28ff7c3a9
Ziggy Baz <basil99magawi@gmail.com.com>
2024-04-06 03:26:00 +0300

    disabling the standardlibrary\ndisabling unwinding to eliminate using language items which are complex and unstable

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+//I'll clean up all this comments after implementation, i hate them goes against the principles of keeping code clean.
+#![no_std]
+
+use core::panic::PanicInfo;
+
+
+fn main() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }

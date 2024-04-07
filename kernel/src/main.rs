f31d672423f5669170af86139f3374f05563683d
Ziggy Baz <basil99magawi@gmail.com.com>
2024-04-07 03:28:33 +0300

    Building the kernel itself.Provide proper documentation for this segment on issues such as architectural decisions such as not writing the bootloader

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -9,7 +9,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub extern "C" fn _start() -> ! { //system entry-point, surprised to learn main isn't it in most languages.
+static OSNAME: &[u88] = b"Ziggy OS Loading";
+
+#[no_mangle] //don't mangle the name of this function instead use 'C' convention
+pub extern "C" fn _start() -> ! { //system entry-point, surprised to learn main isn't it in most languages.(named _start by default, standard)
+   
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in OSNAME.iter().enumerate() {
+        unsafe { //please, this isn't the way we do things in Rust, its only here because rust can't prove the raw pointers created are valid, we use unsafe to calm down the compiler. Don't make a habit of this.
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 

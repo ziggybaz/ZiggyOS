61981b6dac0937d360c45e32a566f189656a8074
Ziggy Baz <basil99magawi@gmail.com>
2024-07-04 17:48:16 +0300

    testing the VGA Buffer

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -57,11 +57,26 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -69,7 +84,6 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("testing assertions...");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
+
